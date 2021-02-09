21e4da2de5ba498c6f8a4fa891dc76cfc0b03bbf
Diarte Jeffcoat <diartesjeffcoat@icloud.com>
2021-02-09 01:51:36 -0500

    Creating the Pokecards through the API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // API Fetch 
 
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const pokemon_count = 200
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -19,6 +19,9 @@ const colors = {
     normal: '#F5F5F5'
 }
 
+const main_types = Object.keys(colors)
+// console.log(main_types)
+
 async function fetchPokemon() {
     for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
@@ -29,7 +32,44 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokeCards(data)
 }
 
 fetchPokemon()
+
+function createPokeCards(pokemon) {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    // console.log(pokemon.types)
+
+    const pokeTypes = pokemon.types.map(type => type.type.name)
+
+    const type = main_types.find(type => pokeTypes.indexOf(type) > -1)
+
+    const type_name = type[0].toUpperCase() + type.slice(1)
+
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+    // console.log(pokeTypes)
+
+    const pokemonInnerTemplate = `
+        <div class="img-container">
+          <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+        </div>
+        <div class="info">
+          <span class="number">#${id}</span>
+          <h3 class="name">${name}</h3>
+          <small class="type">Type: <span>${type_name}</span></small>
+        </div>
+    `
+
+    pokemonEl.innerHTML = pokemonInnerTemplate
+
+    poke_container.appendChild(pokemonEl)
+
+}
